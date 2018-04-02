4d0b1f50e816afe483c71c9b177ab09425608cc8
InviewCompany <mr_a953@mail.ru>
2018-04-02 21:50:29 +0300

    Исправлена ошибка с запуском всех классов

--- src/Console.java
+++ src/Console.java
@@ -1,27 +1,31 @@
+import java.net.UnknownHostException;
 import java.util.Scanner;
-
+import java.net.InetAddress;
 import static java.lang.System.out;
 
 public class Console {
-    public static void main(String args[]){
+    public static void main(String args[]) throws UnknownHostException {
+        String computername=InetAddress.getLocalHost().getHostName();
         Scanner scn=new Scanner(System.in);
         out.println("Добро пожаловать в Chameleon");
         boolean isWork=true;
         while(isWork){
+            out.print(computername+":");
             String command=scn.nextLine();
             switch(command){
 
                     case "/enc":
                     new Encrypt();
-
+                    break;
                     case "/dec":
                     new Decrypt();
-
+                    break;
                     case "/help":
                     new Help();
-
+                    break;
                     case "/exit":
                     isWork=false;
+                    break;
             }
         }
     }
--- src/Help.java
+++ src/Help.java
@@ -3,6 +3,6 @@ import static java.lang.System.out;
 public class Help {
     Help(){
       out.println("/help-вывод списка команд");
-        out.println("/exit - выход из консоли");
+      out.println("/exit - выход из консоли");
     }
 }
