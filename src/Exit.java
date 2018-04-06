1b8a48e302ac7fada3a23a4740617733709239cc
InviewCompany <mr_a953@mail.ru>
2018-04-06 21:11:09 +0300

    Продолжаем работу

---
+++ src/Exit.java
@@ -0,0 +1,52 @@
+import static java.lang.System.out;
+
+public class Exit {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    Exit(){
+        out.println(ANSI_GREEN+"                                                                                                              \n" +
+                "                                                                                                              \n" +
+                "                                                                                                              \n" +
+                "                                                                                                              \n" +
+                "                                                                                                              \n" +
+                "                                                                                                              \n" +
+                "                                                                                                              \n" +
+                "                                                  iX25S55SSS25i                                               \n" +
+                "                                              552XG22532X53G3XX3ii5                                           \n" +
+                "                                         X5hX;Xr2X9BSA2H&h#9h&2AhAXhh9                                        \n" +
+                "                                ;     hiX;&&9555S9hX3SiS3h2AA9;As9G22GH3                                      \n" +
+                "                          s;H9rsS .X9 5G5GX939Xr23hAX2s55GiSSi&iMG3G&2XhhG                                    \n" +
+                "                        Sii:r;sXis3iSXXS33XX2h32X93hh25hh&SH9:HAH229XG2h2h9                                   \n" +
+                "                  Bi53#A#hAXr39ih3:HhG35iGh5h5SX9A3322i5rsiAG2S5SH3r2i93X9955                                 \n" +
+                "                ,9;.iHG#25A5r&S.2;&252r9i2r5i5sh&3G5S2Xs5Hs3Xhh#3#G:2h33XGhXG&                                \n" +
+                "               5:r599H##h;Hrr22i3#G.Srih3ih:39rGG5Hrh&r39ri&95iS92A35isHA3hG223                               \n" +
+                "            ,.s3isis&@925i;SXXShsG#BBih;rr59sh;SH3&92h3h&333BH3#9Ghi5GM9iGX3A359                              \n" +
+                "            5rsi5s .  XSsh2sABA;&GGrsiS329s2;95Hs#hs ,& h A.AMBM@&9S53&&35B&G39sr5                            \n" +
+                "             52Xh2XXX#2A r2X2H3,i.B9#AH hA#Gr;, ; S#A5M@B@5A#@B#MMGXS2BX9SGh&X2S32;                           \n" +
+                "               9SrShAhS;3hh3GX9XshiXh.;Xr 5 9M@3B@ABHGBi@M25BMH5HSH229GMX5iiXG2SXhX25                         \n" +
+                "                 ,&H9s5h&G3h9s559XSX3isSi2i5:2;3MB9B#hAHBA93S&ABAA3h55MBhG&GAGXhS39rSr2                       \n" +
+                "                    .r2hh3;Xi522GihHG&BMhGA2h5sXhBGAXA9GBh&9h9XHA9rXSri&;3BMh&GAh&AS55riSsi2S2233G            \n" +
+                "                         : :2AA39hABAA&XGhh&S3S2Bhhi35 3:    ,. SX@Sis5iAG&H&BABHM#@BAH&H&H&hGGAMsSr;;rSSSi5  \n" +
+                "                                 ,HhA      r.hrH           BG&G9#59s5X2.M@#MAr      9BHM#M@M@#B##M@AHS&&G&rX22\n" +
+                "                                   r9X    s:s&B      3H3h3rGXMMHMs#M 33B              9h3AMHM        &BBG#&HH&\n" +
+                "                                   :;5    32SH    i. 32A&9#MBGG 29X                     i993&B              h&\n" +
+                "                                   , :H  :h59   M&XS&9MB&A                               ;3S5GH               \n" +
+                "                                      9;,iXB HXhhAM#G.,                                   ,5s9GA              \n" +
+                "                                     ;i,:rM&5:BMMH                                         :;s5G              \n" +
+                "                                 :  ;9;is GhM@B                                             ss5X9             \n" +
+                "                                :  s3 Hs,,HH                                                :;SSh             \n" +
+                "                                 :5 GM&XX.r                                                 .S3S&             \n" +
+                "i5i5S                           ;X&&3A#A h9.                                    H:2ihS93    :S2&H             \n" +
+                "si2255XXX2hhhGGG&XXh3     r  rhH9H&A#B                                         ,i5MSS9GiS   ,59Xi             \n" +
+                "  SX99XGG2h993HA9i,G3&&&sSGAs9hGB5MB                                           S;H5Hh S;5G .Sr3A              \n" +
+                "              sG2MH2GGGSH5AA&#BHh                                              S&G22X5Gr9 ,5s9#               \n" +
+                "                      ;XBHB&                                                   ;52i&392& S22S9                \n" +
+                "                       h:G                                                      32;&S2XS5XXGB                 \n" +
+                "                                                                                  @3A2G9hG5                   \n" +
+                "                                                                                                              \n" +
+                "                                                                                                              \n" +
+                "                                                                                                              \n" +
+                "                                                                                                              \n"+ANSI_RESET);
+        System.exit(1);
+    }
+}
